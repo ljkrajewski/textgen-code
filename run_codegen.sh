c58dd50c8ca076d764efa7de466a33ceceb67781
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-10-11 09:26:22 -0400

    Update run_codegen.sh

--- run_codegen.sh
+++ run_codegen.sh
@@ -5,16 +5,19 @@ TGWBASE="$BASE/text-generation-webui"
 MODEL='TheBloke/WizardCoder-15B-1.0-GPTQ'
 MODEL_DIR=$(echo $MODEL | awk -F '/' '{ print $2 }')
 
+## Prereqs
 apt update && apt upgrade git -y && apt install tmux vim psmisc less unzip git-lfs -y
 python -m pip install --upgrade pip
 python -m pip install virtualenv auto-gptq
 
+## Install Oobabooga & start virtual environment
 cd $BASE
 git clone https://github.com/oobabooga/text-generation-webui.git
 cd $TGWBASE
 python -m venv venv
 source venv/bin/activate
 
+## Download model from Hugging Face
 cd $TGWBASE/models
 git lfs install
 git clone https://huggingface.co/$MODEL
@@ -22,8 +25,18 @@ git clone https://huggingface.co/$MODEL
 #cd $MODEL_DIR
 #wget https://huggingface.co/$MODEL/resolve/main/model.safetensors
 
+## Command-line flag options:
+#  https://github.com/oobabooga/text-generation-webui#starting-the-web-ui
 cd $TGWBASE
 #echo "--listen --chat --auto-devices --model llama" >> CMD_FLAGS.txt
-echo "--listen --chat --auto-devices --model $MODEL_DIR" >> CMD_FLAGS.txt
-export GPU_CHOICE='A'
+echo "--listen --chat --auto-devices --cpu --model $MODEL_DIR" >> CMD_FLAGS.txt
+
+## GPU_CHOICE options
+#    A) NVIDIA
+#    B) AMD (Linux/MacOS only. Requires ROCm SDK 5.6 on Linux)
+#    C) Apple M Series
+#    D) Intel Arc (IPEX)
+#    N) None (I want to run models in CPU mode)
+export GPU_CHOICE='N'
+
 bash start_linux.sh
